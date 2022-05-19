58c3d283ad98fe74ab53de6213ff4025560a7c5e
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-05-19 18:20:44 +0900

    ArrayQueue code

--- chap7/ArrayQueue.c
+++ chap7/ArrayQueue.c
@@ -67,3 +67,107 @@ int isArrayQueueFull(ArrayQueue* pQueue)
 
     return ret;
 }
+
+ArrayQueue *dequeueAQ(ArrayQueue* pQueue)
+{
+	ArrayQueueNode *pReturn = NULL;
+	if(pQueue != NULL) {
+		if(isArrayQueueEmpty(pQueue) == 0) {
+			pReturn = (ArrayQueueNode *)malloc(sizeof(ArrayQueueNode));
+			if(pReturn != NULL) {
+				pQueue->front++;
+				pReturn->data = pQueue->pData[ pQueue->front ].data;
+				pQueue->currentCount--;
+			}
+			else {
+				printf("오류, 메모리 할당, dequeueAQ()\n");
+			}
+		}
+	}
+
+	return pReturn;
+}
+
+ArrayQueueNode *peekAQ(ArrayQueue* pQueue)
+{
+	ArrayQueueNode *pReturn = NULL;
+	if(pQueue != NULL) {
+		if(isArrayQueueEmpty(pQueue) == 0) {
+			pReturn = &(pQueue->pData[ pQueue->front + 1 ]);
+		}
+	}
+
+	return pReturn;
+}
+
+int isArrayQueueEmpty(ArrayQueue* pQueue)
+{
+	int ret = 0;
+	if(pQueue != NULL) {
+		if(pQueue->currentCount == 0) {
+			ret = 1;
+		}
+	}
+
+	return ret;
+}
+
+void deleteArrayQueue(ArrayQueue* pQueue)
+{
+	if(pQueue != NULL) {
+		if(pQueue->pData != NULL) {
+			free(pQueue->pData);
+		}
+		free(pQueue);
+	}
+}
+
+void displayArrayQueue(ArrayQueue *pQueue)
+{
+	int i = 0;
+
+	if(pQueue != NULL) {
+		printf("큐의 크기: %d, 현재 노드 개수: %d\n",
+			pQueue->maxCount,
+			pQueue->currentCount);
+
+		for(i = pQueue->front + 1; i <= pQueue->rear; i++) {
+			printf("[%d]-[%c]\n", i, pQueue->pData[ i ].data);
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+    ArrayQueue *pQueue = NULL;
+    ArrayQueueNode *pNode = NULL;
+
+    pQueue = createArrayQueue(4);
+    if(pQueue != NULL) {
+
+        enqueueAQ(pQueue, 'A');
+        enqueueAQ(pQueue, 'B');
+        enqueueAQ(pQueue, 'C');
+        enqueueAQ(pQueue, 'D');
+        displayArrayQueue(pQueue);
+
+        pNode = dequeueAQ(pQueue);
+        if (pNode != NULL) {
+            printf("Dequeue Value-[%c]\n",
+                pNode->data);
+            free( pNode );
+        }
+        displayArrayQueue(pQueue);
+
+        pNode = peekAQ(pQueue);
+        if (pNode != NULL) {
+            printf("Peek Value-[%c]\n",
+                pNode->data);
+        }
+        displayArrayQueue(pQueue);
+
+        enqueueAQ(pQueue, 'E');
+        displayArrayQueue(pQueue);
+    }
+    return 0;
+}
